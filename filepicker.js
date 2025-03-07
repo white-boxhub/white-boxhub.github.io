f5cfe7e29487adec12012525d7c4bdde042881aa
white-boxhub <52767097+white-boxhub@users.noreply.github.com>
2025-03-07 00:23:42 +0000

    Add files via upload

---
+++ filepicker.js
@@ -0,0 +1,28 @@
+window.addEventListener('DOMContentLoaded', function() {
+    
+const apikey = 'AS0N3aF5HScGsyMqxF2MFz';
+const client = filestack.init(apikey); 
+
+var options ={
+ 	 onUploadDone: (res) => console.log(res), 
+
+	"accept": [
+		".pdf",
+
+	],
+	"fromSources": [
+		"local_file_system"
+	],
+		"disableTransformer": true,
+		"maxFiles": 4,
+		"minFiles": 1,
+		"uploadInBackground": false,
+		"maxSize": 1024*1024*50
+};
+
+const picker = client.picker(options); 
+//picker.open();
+
+    const openBtn = this.document.getElementById('open');
+    openBtn.addEventListener('click', () => picker.open());
+});
